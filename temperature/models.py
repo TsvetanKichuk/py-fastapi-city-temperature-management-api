f320608defbbd59d240b0befceaaf5626677fb4c
TsvetanKichuk <toys4babyodua@gmail.com>
2025-03-01 20:00:49 +0100

    Migrate Temperature model to SQLAlchemy ORM.
    Replaced Pydantic's BaseModel with SQLAlchemy's Base and adjusted imports and relationships accordingly. This enables direct integration with the database schema, improving ORM functionality and compatibility.

--- temperature/models.py
+++ temperature/models.py
@@ -1,14 +1,16 @@
-from pydantic.v1 import BaseModel
 from sqlalchemy import Column, Integer, ForeignKey, DateTime, Float
 from sqlalchemy.orm import relationship
 
+from city.models import City
+from database import Base
 
-class Temperature(BaseModel):
+
+class Temperature(Base):
     __tablename__ = "temperature"
 
-    id = Column(Integer, primary_key=True)
-    city_id_ = Column(Integer, ForeignKey("city.id"))
+    id = Column(Integer, primary_key=True, index=True)
+    city_id_ = Column(Integer, ForeignKey("cities.id"))
     date_time = Column(DateTime)
     temperature = Column(Float)
 
-    city = relationship("City")
+    city = relationship(City)
